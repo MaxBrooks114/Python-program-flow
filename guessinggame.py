2d42159e8763d4150de920d7e05e84f1797628ee
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-04-14 14:25:45 -0400

    refactoring

--- guessinggame.py
+++ guessinggame.py
@@ -3,19 +3,32 @@ answer = 5
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("higher")
-    guess = int(input())
-    if guess == answer:
-        print("You got it!")
-    else:
-        print("wrong!")
-elif guess > answer:
-    print("lower")
-    guess = int(input())
-    if guess == answer:
-        print("You got it!")
-    else:
-        print("wrong!")
+if guess == answer:
+    print("Got it on the first try!")
 else:
-    print("You got it!")
+    if guess < answer:
+        print("Higher")
+    else:
+        print("Lower")
+    guess = int(input())
+    if guess == answer:
+        print("You got it!")
+    else:
+        print("wrong!")
+
+# if guess < answer:
+#     print("higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("You got it!")
+#     else:
+#         print("wrong!")
+# elif guess > answer:
+#     print("lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("You got it!")
+#     else:
+#         print("wrong!")
+# else:
+#     print("You got it!")
