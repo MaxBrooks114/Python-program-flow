ed87f3df79f4462fe335ca9c26c90c821cd132cd
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-04-14 14:58:48 -0400

    challenge

---
+++ challenge.py
@@ -0,0 +1,7 @@
+name = input("Name please: ")
+age = int(input("How old are you? "))
+
+if 18 <= age <= 30:
+    print("{}, Welcome to fuck fest 2k20!".format(name))
+else:
+    print("sorry pal not this time")
